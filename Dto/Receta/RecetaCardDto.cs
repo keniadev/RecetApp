a933a495f1a4c392b6f39a984c29ff3800b33d8e
keniadev <kenializbethcontreras@outlook.es>
2025-09-28 13:06:14 -0600

    Cambios en el enpoint de recetas para las cards y los detalles y arreglo de crear receta

---
+++ Dto/Receta/RecetaCardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RecetApp.Dto.Receta
+{
+    public record RecetaCardDto(
+        int Id,
+        string Titulo,
+        string? ImagenPrincipal,
+        string? UsuarioNombre
+    );
+}
